6f4c3079326ca95965568e3936e2c89705fcbe06
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-07-21 10:24:51 +0100

    static files setting

--- hrm_project/settings.py
+++ hrm_project/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 import cloudinary
 from decouple import config
-import rest_framework
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,7 +133,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
